2c1472a92ca89576d66f81ae262988b95463ec86
irolram <rrivan@iesrafaelalberti.es>
2025-02-15 17:43:17 +0100

    Acabado

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,11 @@
 
 fun main() {
 
+    val coche = Automovil1("nose", "nose", "nose", 10)
+    coche.mostrarInformacion()
+    println("Autonomía del automóvil: ${coche.calcularAutonomia()} km")
+
+    val moto = Motocicleta(150, "Honda", "CBR", 15)
+    moto.mostrarInformacion()
+    println("Autonomía de la motocicleta: ${moto.calcularAutonomia()} km")
 }
